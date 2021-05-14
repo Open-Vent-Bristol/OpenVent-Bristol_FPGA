cf00348c55fe07dc0a29e51c0b8f7a37cca585c1
Josh Redford <ford.red@gmail.com>
2021-05-14 04:27:58 +0200

    Add sigma-delta modules to top level

--- src/clocks_and_reset.vhd
+++ src/clocks_and_reset.vhd
@@ -2,98 +2,141 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-
 entity clocks_and_reset is
     generic (
-        BOARD   : string := "OVB"   -- OVB/EVAL
+        BOARD : STRING := "OVB" -- OVB/EVAL
     );
     port (
-        clkin   : in    std_logic;
-        srst    : in    std_logic;  -- Soft Reset
-        clk     : out   std_logic;
-        rst     : out   std_logic;
-        hb      : out   std_logic
+        clkin : in  STD_LOGIC;
+        srst  : in  STD_LOGIC;  -- Soft Reset
+        clk   : out STD_LOGIC;
+        rst   : out STD_LOGIC;
+        hb    : out STD_LOGIC
     );
 end entity;
 
 architecture rtl of clocks_and_reset is
+
+    component rPLL
+        generic (
+            FCLKIN           : in STRING  := "100.0";
+            DEVICE           : in STRING  := "GW1N-4";
+            DYN_IDIV_SEL     : in STRING  := "false";
+            IDIV_SEL         : in INTEGER := 0;
+            DYN_FBDIV_SEL    : in STRING  := "false";
+            FBDIV_SEL        : in INTEGER := 0;
+            DYN_ODIV_SEL     : in STRING  := "false";
+            ODIV_SEL         : in INTEGER := 8;
+            PSDA_SEL         : in STRING  := "0000";
+            DYN_DA_EN        : in STRING  := "false";
+            DUTYDA_SEL       : in STRING  := "1000";
+            CLKOUT_FT_DIR    : in BIT     := '1';
+            CLKOUTP_FT_DIR   : in BIT     := '1';
+            CLKOUT_DLY_STEP  : in INTEGER := 0;
+            CLKOUTP_DLY_STEP : in INTEGER := 0;
+            CLKOUTD3_SRC     : in STRING  := "CLKOUT";
+            CLKFB_SEL        : in STRING  := "internal";
+            CLKOUT_BYPASS    : in STRING  := "false";
+            CLKOUTP_BYPASS   : in STRING  := "false";
+            CLKOUTD_BYPASS   : in STRING  := "false";
+            CLKOUTD_SRC      : in STRING  := "CLKOUT";
+            DYN_SDIV_SEL     : in INTEGER := 2
+        );
+        port (
+            CLKOUT   : out STD_LOGIC;
+            LOCK     : out STD_LOGIC;
+            CLKOUTP  : out STD_LOGIC;
+            CLKOUTD  : out STD_LOGIC;
+            CLKOUTD3 : out STD_LOGIC;
+            RESET    : in STD_LOGIC;
+            RESET_P  : in STD_LOGIC;
+            CLKIN    : in STD_LOGIC;
+            CLKFB    : in STD_LOGIC;
+            FBDSEL   : in STD_LOGIC_VECTOR(5 downto 0);
+            IDSEL    : in STD_LOGIC_VECTOR(5 downto 0);
+            ODSEL    : in STD_LOGIC_VECTOR(5 downto 0);
+            PSDA     : in STD_LOGIC_VECTOR(3 downto 0);
+            DUTYDA   : in STD_LOGIC_VECTOR(3 downto 0);
+            FDLY     : in STD_LOGIC_VECTOR(3 downto 0)
+        );
+    end component;
+
     signal rst_cnt  : natural range 0 to 15 := 0;
     signal locked   : std_logic;
-    signal arst     : std_logic;
-    signal hb_cnt   : natural range 0 to 2**25-1;
+    signal hb_cnt   : natural range 0 to 2**25-1 := 0;
 begin
 
-    p_rst: process(clk)
+    p_rst : process (clk)
     begin
         if rising_edge(clk) then
             if locked = '0' or srst = '1' then
-                rst <= '1';
+                rst     <= '1';
                 rst_cnt <= 0;
             else
                 if rst_cnt = 15 then
                     rst <= '0';
                 else
-                    rst <= '1';
+                    rst     <= '1';
                     rst_cnt <= rst_cnt + 1;
                 end if;
             end if;
         end if;
     end process;
 
-    g_pll: if BOARD = OVB generate
+    g_pll : if BOARD = "OVB" generate
 
-        i_pll: PLL
+        i_pll : rPLL
         generic map (
-            FCLKIN => "33.554",
-            DEVICE => "GW1N-9",
-            DYN_IDIV_SEL => "false",
-            IDIV_SEL => 0,
-            DYN_FBDIV_SEL => "false",
-            FBDIV_SEL => 0,
-            DYN_ODIV_SEL => "false",
-            ODIV_SEL => 8,
-            PSDA_SEL => "0000",
-            DYN_DA_EN => "false",
-            DUTYDA_SEL => "1000",
-            CLKOUT_FT_DIR => '1',
-            CLKOUTP_FT_DIR => '1',
-            CLKOUT_DLY_STEP => 0,
+            FCLKIN           => "33.554",
+            DEVICE           => "GW1N-9",
+            DYN_IDIV_SEL     => "false",
+            IDIV_SEL         => 0,
+            DYN_FBDIV_SEL    => "false",
+            FBDIV_SEL        => 0,
+            DYN_ODIV_SEL     => "false",
+            ODIV_SEL         => 8,
+            PSDA_SEL         => "0000",
+            DYN_DA_EN        => "false",
+            DUTYDA_SEL       => "1000",
+            CLKOUT_FT_DIR    => '1',
+            CLKOUTP_FT_DIR   => '1',
+            CLKOUT_DLY_STEP  => 0,
             CLKOUTP_DLY_STEP => 0,
-            CLKFB_SEL => "internal",
-            CLKOUT_BYPASS => "false",
-            CLKOUTP_BYPASS => "false",
-            CLKOUTD_BYPASS => "false",
-            DYN_SDIV_SEL => 2,
-            CLKOUTD_SRC => "CLKOUT",
-            CLKOUTD3_SRC => "CLKOUT"
+            CLKFB_SEL        => "internal",
+            CLKOUT_BYPASS    => "false",
+            CLKOUTP_BYPASS   => "false",
+            CLKOUTD_BYPASS   => "false",
+            DYN_SDIV_SEL     => 2,
+            CLKOUTD_SRC      => "CLKOUT",
+            CLKOUTD3_SRC     => "CLKOUT"
         )
         port map (
-            CLKOUT => clk,
-            LOCK => locked,
-            CLKOUTP => open,
-            CLKOUTD => open,
+            CLKOUT   => clk,
+            LOCK     => locked,
+            CLKOUTP  => open,
+            CLKOUTD  => open,
             CLKOUTD3 => open,
-            RESET => '0',
-            RESET_P => '0',
-            CLKIN => clkin,
-            CLKFB => '0',
-            FBDSEL => 6x"00",
-            IDSEL => 6x"00",
-            ODSEL => 6x"00",
-            PSDA => 4x"0",
-            DUTYDA => 4x"0",
-            FDLY => 4x"0"
+            RESET    => '0',
+            RESET_P  => '0',
+            CLKIN    => clkin,
+            CLKFB    => '0',
+            FBDSEL   => 6x"00",
+            IDSEL    => 6x"00",
+            ODSEL    => 6x"00",
+            PSDA     => 4x"0",
+            DUTYDA   => 4x"0",
+            FDLY     => 4x"0"
         );
 
-        p_hb: process(clk)
+        p_hb : process (clk)
         begin
             if rising_edge(clk) then
                 if rst = '1' then
                     hb <= '0';
                 else
-                    if hb_cnt = 2**25-1 then
+                    if hb_cnt >= 2**25 - 1 then
                         hb_cnt <= 0;
-                        hb <= not hb;
+                        hb     <= not hb;
                     else
                         hb_cnt <= hb_cnt + 1;
                     end if;
@@ -101,66 +144,67 @@ begin
             end if;
         end process;
 
-    else generate
-        i_pll: PLL
-        generic map (
-            FCLKIN => "50",
-            DEVICE => "GW1N-9",
-            DYN_IDIV_SEL => "false",
-            IDIV_SEL => 2,
-            DYN_FBDIV_SEL => "false",
-            FBDIV_SEL => 1,
-            DYN_ODIV_SEL => "false",
-            ODIV_SEL => 16,
-            PSDA_SEL => "0000",
-            DYN_DA_EN => "true",
-            DUTYDA_SEL => "1000",
-            CLKOUT_FT_DIR => '1',
-            CLKOUTP_FT_DIR => '1',
-            CLKOUT_DLY_STEP => 0,
-            CLKOUTP_DLY_STEP => 0,
-            CLKFB_SEL => "internal",
-            CLKOUT_BYPASS => "false",
-            CLKOUTP_BYPASS => "false",
-            CLKOUTD_BYPASS => "false",
-            DYN_SDIV_SEL => 2,
-            CLKOUTD_SRC => "CLKOUT",
-            CLKOUTD3_SRC => "CLKOUT"
-        )
-        port map (
-            CLKOUT => clk,
-            LOCK => locked,
-            CLKOUTP => open,
-            CLKOUTD => open,
-            CLKOUTD3 => open,
-            RESET => '0',
-            RESET_P => '0',
-            CLKIN => clkin,
-            CLKFB => '0',
-            FBDSEL => 6x"00",
-            IDSEL => 6x"00",
-            ODSEL => 6x"00",
-            PSDA => 4x"0",
-            DUTYDA => 4x"0",
-            FDLY => 4x"0"
-        );
+    else
+        generate
+            i_pll : rPLL
+            generic map (
+                FCLKIN           => "50",
+                DEVICE           => "GW1N-9",
+                DYN_IDIV_SEL     => "false",
+                IDIV_SEL         => 2,
+                DYN_FBDIV_SEL    => "false",
+                FBDIV_SEL        => 1,
+                DYN_ODIV_SEL     => "false",
+                ODIV_SEL         => 16,
+                PSDA_SEL         => "0000",
+                DYN_DA_EN        => "true",
+                DUTYDA_SEL       => "1000",
+                CLKOUT_FT_DIR    => '1',
+                CLKOUTP_FT_DIR   => '1',
+                CLKOUT_DLY_STEP  => 0,
+                CLKOUTP_DLY_STEP => 0,
+                CLKFB_SEL        => "internal",
+                CLKOUT_BYPASS    => "false",
+                CLKOUTP_BYPASS   => "false",
+                CLKOUTD_BYPASS   => "false",
+                DYN_SDIV_SEL     => 2,
+                CLKOUTD_SRC      => "CLKOUT",
+                CLKOUTD3_SRC     => "CLKOUT"
+            )
+            port map (
+                CLKOUT   => clk,
+                LOCK     => locked,
+                CLKOUTP  => open,
+                CLKOUTD  => open,
+                CLKOUTD3 => open,
+                RESET    => '0',
+                RESET_P  => '0',
+                CLKIN    => clkin,
+                CLKFB    => '0',
+                FBDSEL   => 6x"00",
+                IDSEL    => 6x"00",
+                ODSEL    => 6x"00",
+                PSDA     => 4x"0",
+                DUTYDA   => 4x"0",
+                FDLY     => 4x"0"
+            );
 
-        p_hb: process(clk)
-        begin
-            if rising_edge(clk) then
-                if rst = '1' then
-                    hb <= '0';
-                else
-                    if hb_cnt = 33333333-1 then
-                        hb_cnt <= 0;
-                        hb <= not hb;
+            p_hb : process (clk)
+            begin
+                if rising_edge(clk) then
+                    if rst = '1' then
+                        hb <= '0';
                     else
-                        hb_cnt <= hb_cnt + 1;
+                        if hb_cnt >= 33333333 - 1 then
+                            hb_cnt <= 0;
+                            hb     <= not hb;
+                        else
+                            hb_cnt <= hb_cnt + 1;
+                        end if;
                     end if;
                 end if;
-            end if;
-        end process;
+            end process;
 
-    end generate;
+        end generate;
 
-end architecture;
+    end architecture;
--- src/ovb.vhd
+++ src/ovb.vhd
@@ -4,70 +4,175 @@ use ieee.numeric_std.all;
 
 use work.ovb_h.all;
 
-
 entity ovb is
     generic (
-        REV         : std_logic_vector(31 downto 0) := 32x"0";
-        UTC         : std_logic_vector(31 downto 0) := 32x"0"
+        REV : STD_LOGIC_VECTOR(31 downto 0) := 32x"0";
+        UTC : STD_LOGIC_VECTOR(31 downto 0) := 32x"0"
     );
     port (
-        CLCK                : in    std_logic;
-        AD_VREF             : in    std_logic;
-        Vref_DRV            : out   std_logic;
-        Vcap_O2_SENS        : in    std_logic;
-        DRV_O2_SENS         : out   std_logic;
-        Vcap_PRES_SENS_VENT : in    std_logic;
-        DRV_PRES_SENS_VENT  : out   std_logic;
-        Vcap_PRES_SENS_PAT  : in    std_logic;
-        DRV_PRES_SENS_PAT   : out   std_logic;
-        Vcap_FLOW_SENS_DRCT : in    std_logic;
-        DRV_FLOW_SENS       : out   std_logic;
-        Vcap_FLOW_SENS_GAIN : in    std_logic;
-        DRV_FLOW_SENS_GAIN  : out   std_logic;
-        SPK1_SENS           : in    std_logic;
-        SPK2_SENS           : in    std_logic;
-        SPK_HIGH_REF        : in    std_logic;
-        SPK_LOW_REF         : in    std_logic;
-        PB_MINUS            : in    std_logic;
-        PB_MUTE             : in    std_logic;
-        PB_PLUS             : in    std_logic;
-        PB_SEL              : in    std_logic;
-        SPI2_MISO           : in    std_logic;
-        SPI2_MOSI           : out   std_logic;
-        SPI_SCLK            : out   std_logic;
-        SPI2_PRES_CS        : out   std_logic;
-        SPI1_MISO           : out   std_logic;
-        SPI1_MOSI           : in    std_logic;
-        SPI1_SCLK           : in    std_logic;
-        SPI1_FPGA_CS        : in    std_logic;
-        VIN_FAIL_F          : in    std_logic;
-        I2C_SCL             : out   std_logic;
-        I2C_SDA             : inout std_logic;
-        F_DRV               : out   std_logic;
-        G_DRV               : out   std_logic;
-        LED_SERIAL_DRV      : out   std_logic;
-        MOTOR_OFF           : in    std_logic;
-        FPGA_READY          : in    std_logic;
-        SPK1_FLT_N          : in    std_logic;
-        SPK2_FLT_N          : in    std_logic;
-        SPK1_EN             : out   std_logic;
-        SPK2_EN             : out   std_logic;
-        SPK1_IN1            : out   std_logic;
-        SPK1_IN2            : out   std_logic;
-        SPK2_IN1            : out   std_logic;
-        SPK2_IN2            : out   std_logic;
-        RESET_I2C           : out   std_logic;
-        UART_RX             : in    std_logic;
-        UART_TX             : out   std_logic;
-        LCD_A_ENABLE        : out   std_logic;
-        LCD_B_ENABLE        : out   std_logic;
-        LCD_RW              : out   std_logic;
-        LCD_RS              : out   std_logic;
-        LCD_DB              : inout std_logic_vector(7 downto 0)
+        CLCK                : in    STD_LOGIC;
+        AD_VREF             : in    STD_LOGIC;
+        Vref_DRV            : out   STD_LOGIC;
+        Vcap_O2_SENS        : in    STD_LOGIC;
+        DRV_O2_SENS         : out   STD_LOGIC;
+        Vcap_PRES_SENS_VENT : in    STD_LOGIC;
+        DRV_PRES_SENS_VENT  : out   STD_LOGIC;
+        Vcap_PRES_SENS_PAT  : in    STD_LOGIC;
+        DRV_PRES_SENS_PAT   : out   STD_LOGIC;
+        Vcap_FLOW_SENS_DRCT : in    STD_LOGIC;
+        DRV_FLOW_SENS       : out   STD_LOGIC;
+        Vcap_FLOW_SENS_GAIN : in    STD_LOGIC;
+        DRV_FLOW_SENS_GAIN  : out   STD_LOGIC;
+        SPK1_SENS           : in    STD_LOGIC;
+        SPK2_SENS           : in    STD_LOGIC;
+        SPK_HIGH_REF        : in    STD_LOGIC;
+        SPK_LOW_REF         : in    STD_LOGIC;
+        PB_MINUS            : in    STD_LOGIC;
+        PB_MUTE             : in    STD_LOGIC;
+        PB_PLUS             : in    STD_LOGIC;
+        PB_SEL              : in    STD_LOGIC;
+        SPI2_MISO           : in    STD_LOGIC;
+        SPI2_MOSI           : out   STD_LOGIC;
+        SPI_SCLK            : out   STD_LOGIC;
+        SPI2_PRES_CS        : out   STD_LOGIC;
+        SPI1_MISO           : out   STD_LOGIC;
+        SPI1_MOSI           : in    STD_LOGIC;
+        SPI1_SCLK           : in    STD_LOGIC;
+        SPI1_FPGA_CS        : in    STD_LOGIC;
+        VIN_FAIL_F          : in    STD_LOGIC;
+        I2C_SCL             : out   STD_LOGIC;
+        I2C_SDA             : inout STD_LOGIC;
+        F_DRV               : out   STD_LOGIC;
+        G_DRV               : out   STD_LOGIC;
+        LED_SERIAL_DRV      : out   STD_LOGIC;
+        MOTOR_OFF           : in    STD_LOGIC;
+        FPGA_READY          : in    STD_LOGIC;
+        SPK1_FLT_N          : in    STD_LOGIC;
+        SPK2_FLT_N          : in    STD_LOGIC;
+        SPK1_EN             : out   STD_LOGIC;
+        SPK2_EN             : out   STD_LOGIC;
+        SPK1_IN1            : out   STD_LOGIC;
+        SPK1_IN2            : out   STD_LOGIC;
+        SPK2_IN1            : out   STD_LOGIC;
+        SPK2_IN2            : out   STD_LOGIC;
+        RESET_I2C           : out   STD_LOGIC;
+        UART_RX             : in    STD_LOGIC;
+        UART_TX             : out   STD_LOGIC;
+        LCD_A_ENABLE        : out   STD_LOGIC;
+        LCD_B_ENABLE        : out   STD_LOGIC;
+        LCD_RW              : out   STD_LOGIC;
+        LCD_RS              : out   STD_LOGIC;
+        LCD_DB              : inout STD_LOGIC_VECTOR(7 downto 0)
     );
 end entity;
 
-
 architecture rtl of ovb is
+    signal clk                  : STD_LOGIC;
+    signal rst                  : STD_LOGIC;
+    signal hb                   : STD_LOGIC;
+
+    signal O2_SENS_VALID        : STD_LOGIC;
+    signal O2_SENS_DATA         : STD_LOGIC_VECTOR(13 downto 0);
+    signal PRES_SENS_VENT_VALID : STD_LOGIC;
+    signal PRES_SENS_VENT_DATA  : STD_LOGIC_VECTOR(13 downto 0);
+    signal PRES_SENS_PAT_VALID  : STD_LOGIC;
+    signal PRES_SENS_PAT_DATA   : STD_LOGIC_VECTOR(13 downto 0);
+    signal FLOW_SENS_DRCT_VALID : STD_LOGIC;
+    signal FLOW_SENS_DRCT_DATA  : STD_LOGIC_VECTOR(13 downto 0);
+    signal FLOW_SENS_GAIN_VALID : STD_LOGIC;
+    signal FLOW_SENS_GAIN_DATA  : STD_LOGIC_VECTOR(13 downto 0);
 begin
+
+    clocks_and_reset_i : entity work.clocks_and_reset
+        generic map (
+            BOARD => "OVB"
+        )
+        port map (
+            clkin => CLCK,
+            srst  => '0', -- todo, add soft reset signal
+            clk   => clk,
+            rst   => rst,
+            hb    => hb
+        );
+
+    sigmadelta_adc_O2_SENS_i : entity work.sigmadelta_adc
+        generic map (
+            ADC_WIDTH      => 14, -- ADC Convertor Bit Precision
+            ACCUM_BITS     => 14, -- 2^ACCUM_BITS is decimation rate of accumulator
+            LPF_DEPTH_BITS => 3   -- 2^LPF_DEPTH_BITS is decimation rate of averager
+        )
+        port map (
+            clk         => clk,          -- sample rate clock
+            rst         => rst,          -- sync reset, asserted high
+            analog_cmp  => Vcap_O2_SENS, -- input from LVDS buffer (comparator)
+
+            digital_out => O2_SENS_DATA, -- digital output word of ADC
+            analog_out  => DRV_O2_SENS,  -- feedback to comparitor input RC circuit
+            sample_rdy  => O2_SENS_VALID -- digital_out is ready
+        );
+
+    sigmadelta_adc_PRES_SENS_VENT_i : entity work.sigmadelta_adc
+        generic map (
+            ADC_WIDTH      => 14, -- ADC Convertor Bit Precision
+            ACCUM_BITS     => 14, -- 2^ACCUM_BITS is decimation rate of accumulator
+            LPF_DEPTH_BITS => 3   -- 2^LPF_DEPTH_BITS is decimation rate of averager
+        )
+        port map (
+            clk         => clk,                 -- sample rate clock
+            rst         => rst,                 -- sync reset, asserted high
+            analog_cmp  => Vcap_PRES_SENS_VENT, -- input from LVDS buffer (comparator)
+
+            digital_out => PRES_SENS_VENT_DATA, -- digital output word of ADC
+            analog_out  => DRV_PRES_SENS_VENT,  -- feedback to comparitor input RC circuit
+            sample_rdy  => PRES_SENS_VENT_VALID -- digital_out is ready
+        );
+
+    sigmadelta_adc_PRES_SENS_PAT_i : entity work.sigmadelta_adc
+        generic map (
+            ADC_WIDTH      => 14, -- ADC Convertor Bit Precision
+            ACCUM_BITS     => 14, -- 2^ACCUM_BITS is decimation rate of accumulator
+            LPF_DEPTH_BITS => 3   -- 2^LPF_DEPTH_BITS is decimation rate of averager
+        )
+        port map (
+            clk         => clk,                -- sample rate clock
+            rst         => rst,                -- sync reset, asserted high
+            analog_cmp  => Vcap_PRES_SENS_PAT, -- input from LVDS buffer (comparator)
+
+            digital_out => PRES_SENS_PAT_DATA, -- digital output word of ADC
+            analog_out  => DRV_PRES_SENS_PAT,  -- feedback to comparitor input RC circuit
+            sample_rdy  => PRES_SENS_PAT_VALID -- digital_out is ready
+        );
+
+    sigmadelta_adc_FLOW_SENS_DRCT_i : entity work.sigmadelta_adc
+        generic map (
+            ADC_WIDTH      => 14, -- ADC Convertor Bit Precision
+            ACCUM_BITS     => 14, -- 2^ACCUM_BITS is decimation rate of accumulator
+            LPF_DEPTH_BITS => 3   -- 2^LPF_DEPTH_BITS is decimation rate of averager
+        )
+        port map (
+            clk         => clk,                 -- sample rate clock
+            rst         => rst,                 -- sync reset, asserted high
+            analog_cmp  => Vcap_FLOW_SENS_DRCT, -- input from LVDS buffer (comparator)
+
+            digital_out => FLOW_SENS_DRCT_DATA, -- digital output word of ADC
+            analog_out  => DRV_FLOW_SENS,       -- feedback to comparitor input RC circuit
+            sample_rdy  => FLOW_SENS_DRCT_VALID -- digital_out is ready
+        );
+
+    sigmadelta_adc_FLOW_SENS_GAIN_i : entity work.sigmadelta_adc
+        generic map (
+            ADC_WIDTH      => 14, -- ADC Convertor Bit Precision
+            ACCUM_BITS     => 14, -- 2^ACCUM_BITS is decimation rate of accumulator
+            LPF_DEPTH_BITS => 3   -- 2^LPF_DEPTH_BITS is decimation rate of averager
+        )
+        port map (
+            clk         => clk,                 -- sample rate clock
+            rst         => rst,                 -- sync reset, asserted high
+            analog_cmp  => Vcap_FLOW_SENS_GAIN, -- input from LVDS buffer (comparator)
+
+            digital_out => FLOW_SENS_GAIN_DATA, -- digital output word of ADC
+            analog_out  => DRV_FLOW_SENS_GAIN,  -- feedback to comparitor input RC circuit
+            sample_rdy  => FLOW_SENS_GAIN_VALID -- digital_out is ready
+        );
+
 end architecture;
